8ae2b9cf320ee826bf04cd9708305c685907d1ea
godsenal <tmqps78@gmail.com>
2021-03-27 18:05:44 +0900

    amount private 으로 변경

--- src/Dollar.ts
+++ src/Dollar.ts
@@ -1,13 +1,13 @@
 class Dollar {
-  amount = 0;
+  #amount = 0;
   constructor(amount: number) {
-    this.amount = amount;
+    this.#amount = amount;
   }
   times(multiply: number) {
-    return new Dollar(this.amount * multiply);
+    return new Dollar(this.#amount * multiply);
   }
   isEqual(dollar: Dollar) {
-    return this.amount === dollar.amount;
+    return this.#amount === dollar.#amount;
   }
 }
 
--- src/__tests__/Dollar.test.ts
+++ src/__tests__/Dollar.test.ts
@@ -3,18 +3,9 @@ import Dollar from "../Dollar";
 describe("Dollar 테스트", () => {
   it("multiplication 테스트", () => {
     const dollar = new Dollar(5);
-    const product = dollar.times(2);
 
-    expect(product.amount).toBe(10);
-  });
-
-  it("side effect가 없어야 함", () => {
-    const dollar = new Dollar(5);
-    let product = dollar.times(2);
-
-    expect(product.amount).toBe(10);
-    product = dollar.times(3);
-    expect(product.amount).toBe(15);
+    expect(new Dollar(10).isEqual(dollar.times(2))).toBe(true);
+    expect(new Dollar(15).isEqual(dollar.times(3))).toBe(true);
   });
 
   it("equality 테스트", () => {
