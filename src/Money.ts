4624bb4fb33d5ab6634a1a89f320ad11cc36e695
godsenal <tmqps78@gmail.com>
2021-03-27 18:55:05 +0900

    Money, times 을 추상클래스/메서드로 변경

--- src/Money.ts
+++ src/Money.ts
@@ -1,9 +1,13 @@
-class Money {
+import Dollar from "./Dollar";
+
+abstract class Money {
   protected amount = 0;
 
   constructor(amount: number) {
     this.amount = amount;
   }
+
+  abstract times(multiply: number): Money;
 
   isEqual(money: Money) {
     return (
