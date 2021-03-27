5a6a8830894134c2f117c0db0dce6a28a19b7425
godsenal <tmqps78@gmail.com>
2021-03-27 23:17:43 +0900

    Sum 클래스의 times 기능 추가

--- src/__tests__/Money.test.ts
+++ src/__tests__/Money.test.ts
@@ -71,4 +71,17 @@ describe("Money 테스트", () => {
     const result = bank.reduce(sum, "USD");
     expect(Money.dollar(15).isEqual(result)).toBe(true);
   });
+
+  it("Sum 클래스의 times 테스트", () => {
+    const fiveBucks = Money.dollar(5);
+    const tenFrancs = Money.franc(10);
+    const bank = new Bank();
+
+    bank.addRate("CHF", "USD", 2);
+
+    const sum = new Sum(fiveBucks, tenFrancs).times(2);
+    const result = bank.reduce(sum, "USD");
+
+    expect(Money.dollar(20).isEqual(result)).toBe(true);
+  });
 });
