f60fa036164938d0ce41351841653768e455803d
godsenal <tmqps78@gmail.com>
2021-04-04 16:10:04 +0900

    :green_circle: TestCase 클래스를 이용하여 테스트하도록 수정

--- src/test/__tests__/test.ts
+++ src/test/__tests__/test.ts
@@ -1,6 +1,13 @@
+import TestCase from "../TestCase";
 import WasRun from "../WasRun";
 
-const test = new WasRun("testMethod");
-console.log(test.wasRun);
-test.run();
-console.log(test.wasRun);
+class TestCaseTest extends TestCase {
+  testRunning() {
+    const test = new WasRun("testMethod");
+    console.log(test.wasRun);
+    test.run();
+    console.log(test.wasRun);
+  }
+}
+
+new TestCaseTest("testRunning").run();
