a1dc752067c371ca633fa8a186b6d3f6f044ed0d
godsenal <tmqps78@gmail.com>
2021-04-04 17:01:20 +0900

    :recycle: TestResult에 failureCount 추가하여 실패횟수 출력하도록 수정

--- src/test/TestResult.ts
+++ src/test/TestResult.ts
@@ -1,13 +1,18 @@
 class TestResult {
   runCount: number;
+  failureCount: number;
   constructor() {
     this.runCount = 0;
+    this.failureCount = 0;
   }
   testStarted() {
     this.runCount += 1;
   }
+  testFailed() {
+    this.failureCount += 1;
+  }
   summary() {
-    return `${this.runCount} run, 0 failed`;
+    return `${this.runCount} run, ${this.failureCount} failed`;
   }
 }
 
