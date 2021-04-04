2e6db48e97248939ae0ff6e7166dca8fffeeeaf8
godsenal <tmqps78@gmail.com>
2021-04-04 16:35:34 +0900

    :green_circle: test 메서드 실행시 로그 설정하도록 수정

--- src/test/WasRun.ts
+++ src/test/WasRun.ts
@@ -3,6 +3,7 @@ import TestCase from "./TestCase";
 class WasRun extends TestCase {
   wasRun: number | null;
   wasSetUp: number | null;
+  log: string = "";
 
   constructor(name: string) {
     super(name);
@@ -13,10 +14,12 @@ class WasRun extends TestCase {
   setUp() {
     this.wasRun = null;
     this.wasSetUp = 1;
+    this.log = "setUp ";
   }
 
   testMethod() {
     this.wasRun = 1;
+    this.log = this.log + "testMethod";
   }
 }
 
